49bdbca5a7ee4018f25f29b1904b835aa1be48bd
David Denton <denton.david@gmail.com>
2018-05-05 08:55:24 +0100

    slight rework

--- src/main/kotlin/org/protokruft/Generator.kt
+++ src/main/kotlin/org/protokruft/Generator.kt
@@ -2,21 +2,31 @@ package org.protokruft
 
 import com.google.protobuf.GeneratedMessageV3
 import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FileSpec.Builder
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.LambdaTypeName
 import com.squareup.kotlinpoet.asClassName
 import com.squareup.kotlinpoet.asTypeName
 import kotlin.reflect.KClass
 
-class Generator(private val pkg: Package, private val file: String) {
+fun <T : GeneratedMessageV3> Builder.populateFor(clz: KClass<T>) =
+        apply {
+            addFunction(
+                    FunSpec.builder(clz.java.simpleName.decapitalize()).apply {
+                        addParameter("fn", LambdaTypeName.get(
+                                receiver = clz.nestedClasses.find { it.simpleName == "Builder" }!!.asClassName(),
+                                returnType = Unit::class.asTypeName()))
+                        addStatement("return ${clz.qualifiedName}.newBuilder().apply(fn).build()")
+                        returns(clz)
+                    }.build()
+            )
+        }
 
-    fun <T : GeneratedMessageV3> forClass(clz: KClass<T>) = FileSpec.builder(pkg.name, file)
-            .addFunction(FunSpec.builder(clz.java.simpleName.decapitalize())
-                    .addParameter("fn", LambdaTypeName.get(
-                            receiver = clz.nestedClasses.find { it.simpleName == "Builder" }!!.asClassName(),
-                            returnType = Unit::class.asTypeName()))
-                    .addStatement("return ${clz.qualifiedName}.newBuilder().apply(fn).build()")
-                    .returns(clz)
-                    .build())
-            .build()
+class Generator(private val pkg: Package, private val file: String) {
+    fun <T : GeneratedMessageV3> forClass(vararg clz: KClass<T>) =
+            FileSpec.builder(pkg.name, file).apply {
+                clz.forEach {
+                    populateFor(it)
+                }
+            }.build()
 }
