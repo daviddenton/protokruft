9d4cd6b88cf5f8bf6ff7233649d9165979d02c3a
David Denton <denton.david@gmail.com>
2018-05-05 14:42:56 +0100

    working backwards to get the plugin to work...

---
+++ src/main/kotlin/org/gradle/plugin/GenerateProtobufDslPlugin.kt
@@ -0,0 +1,23 @@
+package org.gradle.plugin
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.TaskAction
+
+class GenerateProtobufDslPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        target.tasks.create(TASK_NAME, ATask::class.java)
+    }
+
+    companion object {
+        internal val TASK_NAME = "yourTask"
+    }
+}
+
+open class ATask : DefaultTask() {
+    @TaskAction
+    fun generateClasses() {
+        System.out.printf("HELLO!")
+    }
+}
