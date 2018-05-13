a039355af05c43c3553793c923f9844b53c54995
David Denton <denton.david@gmail.com>
2018-05-13 17:25:36 +0100

    removed example as not needed anymore

--- src/example/kotlin/RunGeneration.kt
+++
@@ -1,9 +0,0 @@
-import org.protokruft.GenerateProtobufDsl
-import org.protokruft.ScanClasspath
-import java.io.File
-
-fun main(args: Array<String>) {
-    GenerateProtobufDsl
-            .generate(ScanClasspath("org.protokruft"), "dsl")
-            .forEach { it.writeTo(File("src/example/kotlin")) }
-}
