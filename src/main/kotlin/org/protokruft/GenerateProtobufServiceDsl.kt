36facb58be48b1145db257c0f7e683cdb69c4d4d
David Denton <denton.david@gmail.com>
2018-07-02 12:03:15 +0100

    moving towards generating service code

--- src/main/kotlin/org/protokruft/GenerateProtobufServiceDsl.kt
+++ src/main/kotlin/org/protokruft/GenerateProtobufServiceDsl.kt
@@ -3,7 +3,11 @@ package org.protokruft
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FileSpec.Builder
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier.ABSTRACT
 import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.asTypeName
+import java.lang.reflect.Modifier
 
 typealias TargetServiceClasses = () -> Iterable<ClassName>
 
@@ -11,16 +15,28 @@ object GenerateProtobufServiceDsl {
     fun generate(
             classNames: TargetServiceClasses,
             outputFilename: String,
-            nameFn: (ClassName) -> String = { "new${it.toSimpleNames()}" }
+            nameFn: (ClassName) -> String = { it.toSimpleNames().replace("BlockingStub", "") }
     ): List<FileSpec> {
         fun Builder.generateFunctionFor(clz: ClassName) =
                 apply {
-                    addType(TypeSpec.objectBuilder(nameFn(clz)).apply {
-                        // generate code here!
+                    val methods = Class.forName(clz.reflectionName()).declaredMethods.filter { Modifier.isPublic(it.modifiers) }
+                    addType(TypeSpec.interfaceBuilder(nameFn(clz)).apply {
+                        methods.forEach {
+                            addFunction(
+                                    FunSpec.builder(it.name).apply {
+                                        addModifiers(ABSTRACT)
+                                        it.parameterTypes.forEach {
+                                            addParameter(it.simpleName, it.asTypeName())
+                                        }
+                                        returns(it.returnType.asTypeName())
+                                    }.build()
+                            )
+                        }
                     }.build())
                 }
 
         return classNames()
+                .filter { it.simpleName().endsWith("BlockingStub") }
                 .groupBy { it.packageName() }
                 .map { (pkg, classes) ->
                     FileSpec.builder(pkg, outputFilename).apply {
--- src/test/kotlin/org/protokruft/GenerateProtobufServiceDslTest.kt
+++ src/test/kotlin/org/protokruft/GenerateProtobufServiceDslTest.kt
@@ -9,10 +9,9 @@ class GenerateProtobufServiceDslTest {
     @Test
     fun `generates expected output for multiple packages`() {
         val generated = GenerateProtobufServiceDsl.generate(ScanClasspathFor("org.protokruft", AbstractStub::class.java), "name")
-
         assertThat(generated.size, equalTo(3))
-//        check(1, generated, "expectedService")
-//        check(2, generated, "expectedService")
-//        check(3, generated, "expectedService")
+        check(1, generated, "expectedService")
+        check(2, generated, "expectedService")
+        check(3, generated, "expectedService")
     }
 }
