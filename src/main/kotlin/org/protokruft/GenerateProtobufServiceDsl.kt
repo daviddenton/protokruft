2f5dfee2f676a4abebc0b176fac492ac74d7fd00
David Denton <denton.david@gmail.com>
2018-07-02 12:48:16 +0100

    moving towards generating service code

--- src/main/kotlin/org/protokruft/GenerateProtobufServiceDsl.kt
+++ src/main/kotlin/org/protokruft/GenerateProtobufServiceDsl.kt
@@ -5,8 +5,11 @@ import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FileSpec.Builder
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.KModifier.ABSTRACT
+import com.squareup.kotlinpoet.KModifier.PRIVATE
+import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
 import com.squareup.kotlinpoet.asTypeName
+import io.grpc.Channel
 import java.lang.reflect.Modifier
 
 typealias TargetServiceClasses = () -> Iterable<ClassName>
@@ -20,7 +23,8 @@ object GenerateProtobufServiceDsl {
         fun Builder.generateFunctionFor(clz: ClassName) =
                 apply {
                     val methods = Class.forName(clz.reflectionName()).declaredMethods.filter { Modifier.isPublic(it.modifiers) }
-                    addType(TypeSpec.interfaceBuilder(nameFn(clz)).apply {
+                    val interfaceName = nameFn(clz)
+                    addType(TypeSpec.interfaceBuilder(interfaceName).apply {
                         methods.forEach {
                             addFunction(
                                     FunSpec.builder(it.name).apply {
@@ -32,6 +36,31 @@ object GenerateProtobufServiceDsl {
                                     }.build()
                             )
                         }
+
+                        addType(
+                                TypeSpec.classBuilder("Grpc")
+                                        .apply {
+                                            addFunction(FunSpec.constructorBuilder().apply {
+                                                addParameter("channel", Channel::class.asTypeName())
+                                                        .addStatement("stub = ${interfaceName}BlockingStub(channel)")
+                                            }.build())
+                                            addSuperinterface(ClassName.bestGuess(interfaceName))
+                                            addProperty(PropertySpec.builder("stub", Channel::class.asTypeName()).addModifiers(PRIVATE).build())
+                                            methods.forEach {
+                                                addFunction(
+                                                        FunSpec.builder(it.name).apply {
+                                                            it.parameterTypes.forEach {
+                                                                addParameter(it.simpleName, it.asTypeName())
+                                                            }
+                                                            returns(it.returnType.asTypeName())
+                                                        }
+                                                                .addStatement("return stub.${it.name}(${it.parameterTypes.map { it.simpleName }.joinToString(",")}})")
+                                                                .build()
+                                                )
+                                            }
+                                        }
+                                        .build()
+                        )
                     }.build())
                 }
 
