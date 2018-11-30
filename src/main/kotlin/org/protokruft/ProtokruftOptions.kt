af2846be1837e9c6c4523bf57690541a595d5afd
David Denton <denton.david@gmail.com>
2018-11-30 19:15:44 +0000

    removed extra constructor and replaced with standard options

--- src/main/kotlin/org/protokruft/ProtokruftOptions.kt
+++ src/main/kotlin/org/protokruft/ProtokruftOptions.kt
@@ -3,11 +3,9 @@ package org.protokruft
 import org.gradle.api.Project
 import java.io.File
 
-open class ProtokruftOptions(var messagesClassFile: String,
-                             var servicesClassFile: String,
-                             var messageDslPrefix: String,
-                             var serviceDslSuffix: String,
-                             var packageNames: Set<String>?,
-                             var outputDirectory: (Project) -> File) {
-    constructor() : this("messageDsl", "serviceDsl", "new", "", null, { File(it.buildDir, "/generated/source/proto/main/java") })
-}
+open class ProtokruftOptions(var messagesClassFile: String = "messageDsl",
+                             var servicesClassFile: String = "serviceDsl",
+                             var messageDslPrefix: String = "new",
+                             var serviceDslSuffix: String = "",
+                             var packageNames: Set<String>? = null,
+                             var outputDirectory: (Project) -> File = { File(it.buildDir, "/generated/source/proto/main/java") })
