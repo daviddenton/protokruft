1d41b32393f6c9e6d32df8cfae5f672b300d8e4c
David Denton <denton.david@gmail.com>
2019-05-30 16:22:40 +0100

    made timeout configurable in client

--- src/main/kotlin/org/protokruft/codeBuilders/grpcClient.kt
+++ src/main/kotlin/org/protokruft/codeBuilders/grpcClient.kt
@@ -4,18 +4,25 @@ import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.KModifier.OPERATOR
 import com.squareup.kotlinpoet.KModifier.OVERRIDE
+import com.squareup.kotlinpoet.ParameterSpec
 import com.squareup.kotlinpoet.TypeSpec
 import com.squareup.kotlinpoet.asTypeName
 import io.grpc.Channel
 import org.protokruft.GrpcService
+import java.time.Duration
+import java.util.concurrent.TimeUnit
 
 fun buildGrpcClient(service: GrpcService, interfaceName: ClassName) =
         TypeSpec.objectBuilder("Client").apply {
             addFunction(FunSpec.builder("invoke").apply {
                 addModifiers(OPERATOR)
                 addParameter("channel", Channel::class.asTypeName())
+                addParameter(ParameterSpec.builder("timeout", Duration::class.asTypeName())
+                    .defaultValue("Duration.ofSeconds(10)")
+                    .build())
                 returns(interfaceName)
-                addStatement("val stub = ${service.className.simpleName}.newBlockingStub(channel)")
+                addStatement("val stub = ${service.className.simpleName}.newBlockingStub(channel).withDeadlineAfter(" +
+                    "timeout.toMillis(), %T.MILLISECONDS)", TimeUnit::class.java)
                 addStatement("return ${TypeSpec.anonymousClassBuilder().apply {
                     addSuperinterface(interfaceName)
                     service.methods.forEach {
