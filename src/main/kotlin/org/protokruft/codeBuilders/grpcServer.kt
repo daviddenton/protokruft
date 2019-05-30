c65e6ddce1e01589d46e45b566af523a09cca5ae
David Denton <denton.david@gmail.com>
2019-05-30 15:56:03 +0100

    remove commented out stuff

--- src/main/kotlin/org/protokruft/codeBuilders/grpcServer.kt
+++ src/main/kotlin/org/protokruft/codeBuilders/grpcServer.kt
@@ -35,33 +35,4 @@ fun buildGrpcServer(service: GrpcService, serviceName: String, interfaceName: Cl
             }.build()}")
         }.build())
     }.build()
-
-//    return TypeSpec.classBuilder("Server").apply {
-//        superclass(service.className.nestedClass(serviceName + "ImplBase"))
-//        primaryConstructor(
-//                FunSpec.constructorBuilder().addParameter("delegate", interfaceName).build()
-//        )
-//        addProperty(
-//                PropertySpec.builder("delegate", interfaceName).addModifiers(PRIVATE).initializer("delegate").build()
-//        )
-//        service.methods.forEach {
-//            addFunction(
-//                    FunSpec.builder(it.name).apply {
-//                        addModifiers(OVERRIDE)
-//                        it.parameters.forEach {
-//                            addParameter(it.simpleName.decapitalize(), it)
-//                        }
-//                        addParameter(
-//                                ParameterSpec.builder(
-//                                        "responseObserver",
-//                                        ClassName("io.grpc.stub", "StreamObserver").parameterizedBy(it.returnType)
-//                                ).build()
-//                        )
-//                        val params = it.parameters.joinToString(", ") { it.simpleName.decapitalize() }
-//                        addStatement("responseObserver.onNext(delegate.${it.name}($params))")
-//                        addStatement("responseObserver.onCompleted()")
-//                    }.build()
-//            )
-//        }
-//    }.build()
 }
